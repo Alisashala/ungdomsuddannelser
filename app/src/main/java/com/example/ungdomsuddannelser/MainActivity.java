a14a877cd5b0870cfc11d79c707d45b01ce2e20f
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-04-18 11:53:10 +0200

    menu tilføjelser

--- app/src/main/java/com/example/ungdomsuddannelser/MainActivity.java
+++ app/src/main/java/com/example/ungdomsuddannelser/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickedButton(View view) {
-        Intent toMenu = new Intent(this, InterestsActivity.class);
+        Intent toMenu = new Intent(this, MenuActivity.class);
         startActivity(toMenu);
     }
 }
--- app/src/main/java/com/example/ungdomsuddannelser/MenuActivity.java
+++ app/src/main/java/com/example/ungdomsuddannelser/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.ungdomsuddannelser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void educationInfo(View view) {
+
+    }
+
+    public void InterestInfo(View view) {
+        Intent toInterest = new Intent(this,InterestsActivity.class);
+        startActivity(toInterest);
     }
 }
