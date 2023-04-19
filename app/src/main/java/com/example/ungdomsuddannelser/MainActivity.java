5e9d3494990a568aa9563345fa97cc178d75c13e
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-04-19 12:33:11 +0200

    firebase connection

--- app/src/main/java/com/example/ungdomsuddannelser/MainActivity.java
+++ app/src/main/java/com/example/ungdomsuddannelser/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.ungdomsuddannelser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addProgram();
+
     }
 
     public void clickedButton(View view) {
@@ -21,4 +33,39 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-}
+
+
+
+
+    public void addProgram(){
+
+        StudyProgramme program1 = new StudyProgramme("International musik", "Kort beskrivelse","Kunst");
+
+
+        ArrayList<StudyProgramme> studyProgrammes = new ArrayList<>();
+        studyProgrammes.add(program1);
+        HashMap<String,Object> data = new HashMap<>();
+        data.put("programme", studyProgrammes);
+
+        // Add a new document with a generated ID
+        db.collection("Studyprogrammes")
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("added");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("error");
+                        System.out.println(e);
+                    }
+                });
+
+
+
+
+}}
+
--- app/src/main/java/com/example/ungdomsuddannelser/StudyProgramme.java
+++ app/src/main/java/com/example/ungdomsuddannelser/StudyProgramme.java
@@ -1,10 +1,49 @@
 package com.example.ungdomsuddannelser;
-
 import java.util.ArrayList;
 
 public class StudyProgramme {
     private String name;
     private String description;
-    private ArrayList courses;
+    private String category;
+    private ArrayList<String> courseList;
 
+    public StudyProgramme(String name, String description, String category) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.courseList = courseList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(ArrayList<String> courseList) {
+        this.courseList = courseList;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
+
