3264f498d0a1ac26941fa1202c4c619100401b16
Alisashala <alis0216@tud.kea.dk>
2023-04-19 12:32:21 +0200

    Forside design

--- app/src/main/java/com/example/ungdomsuddannelser/SprogActivity.java
+++ app/src/main/java/com/example/ungdomsuddannelser/SprogActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class SprogActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,6 @@ public class SprogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sprog);
     }
+
+
 }
