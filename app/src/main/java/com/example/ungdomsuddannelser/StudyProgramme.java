68dc910d3313552495e85b799811c0722ce4f1dd
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-04-19 11:11:36 +0200

    file name change

---
+++ app/src/main/java/com/example/ungdomsuddannelser/StudyProgramme.java
@@ -0,0 +1,10 @@
+package com.example.ungdomsuddannelser;
+
+import java.util.ArrayList;
+
+public class StudyProgramme {
+    private String name;
+    private String description;
+    private ArrayList courses;
+
+}
